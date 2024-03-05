f32164b7bdcb0849fca5b1bfec2ef0de2f050a0e
KalyaginVasiliy <68701581+KalyaginVasiliy@users.noreply.github.com>
2024-03-05 09:53:19 +0300

    Вывод результатов в сессию

--- AJAX.php
+++ AJAX.php
@@ -1,18 +1,20 @@
 <?php
-    require_once ("classes/Connect.php");                                                           // подгружаем класс Connect
-    require_once ("classes/ForCalculations.php");                                                   // подгружаем класс ForCalculations
-    $connect = new Connect ();                                                                      // делаем ассоциацию переменной сonnect с классом Connect
-    $forCalculations = new ForCalculations;                                                         // делаем ассоциацию переменной forCalculations с классом ForCalculations
-    $numberDownloadsFromDatabase = 7;                                                               // определяем количество записей из базы данных, которые будем отображать в конечном результате
-    if (!empty($_REQUEST["numberToEnterOne"]) && (!empty($_REQUEST["numberToEnterTwo"]))) {         // проверяем на непустые значения в полях чисел
-        $numberOne = $_REQUEST["numberToEnterOne"];                                                 // считываем значение первого числа
-        $numberTwo = $_REQUEST["numberToEnterTwo"];                                                 // считываем значение второго числа
-        $calcOperation = $forCalculations->transform($_REQUEST["operationSelect"]);                 // считываем значение операции в выпадающем списке и сразу присваиваем переменной calcOperation 
-                                                                                                    // значение в виде алгебраических действий
-        $calcResultDataBase = $forCalculations->calculate($numberOne, $numberTwo, $calcOperation);  // производим необходимые вычисления с помощью функции calculate
-        $connect->saveResultToDataBase($numberOne, $calcOperation, $numberTwo, $calcResultDataBase);// соединяемся с базой данных и записываем туда значения текущей арифметической операции
-        $operationDataBase = $connect->loadResultsFromDataBase($numberDownloadsFromDatabase);       // загружаем из базы данных необходимое количество записей для отображения, преобразуем запись 
-                                                                                                    // в удобную для чтения строки и складываем их в переменную operationDataBase
+    session_start();                                                                                        // открываем сессию
+    require_once ("classes/Connect.php");                                                                   // подгружаем класс Connect
+    require_once ("classes/ForCalculations.php");                                                           // подгружаем класс ForCalculations
+    $connect = new Connect ();                                                                              // делаем ассоциацию переменной сonnect с классом Connect
+    $forCalculations = new ForCalculations;                                                                 // делаем ассоциацию переменной forCalculations с классом ForCalculations
+    $numberDownloadsFromDatabase = 7;                                                                       // определяем количество записей из базы данных, которые будем отображать в конечном результате
+    if (!empty($_REQUEST["numberToEnterOne"]) && (!empty($_REQUEST["numberToEnterTwo"]))) {                 // проверяем на непустые значения в полях чисел
+        $numberOne = $_REQUEST["numberToEnterOne"];                                                         // считываем значение первого числа
+        $numberTwo = $_REQUEST["numberToEnterTwo"];                                                         // считываем значение второго числа
+        $calcOperation = $forCalculations->transform($_REQUEST["operationSelect"]);                         // считываем значение операции в выпадающем списке и сразу присваиваем переменной calcOperation 
+                                                                                                            // значение в виде алгебраических действий
+        $calcResultDataBase = $forCalculations->calculate($numberOne, $numberTwo, $calcOperation);          // производим необходимые вычисления с помощью функции calculate
+        $connect->saveResultToDataBase($numberOne, $calcOperation, $numberTwo, $calcResultDataBase);        // соединяемся с базой данных и записываем туда значения текущей арифметической операции    
+        $_SESSION['Operation'] = [$numberOne . " " . $calcOperation . " " . $numberTwo . " " . " = " . $calcResultDataBase];       // запоминаем текущий результат в сессию 
+        $operationDataBase = $connect->loadResultsFromDataBase($numberDownloadsFromDatabase);               // загружаем из базы данных необходимое количество записей для отображения, преобразуем запись 
+                                                                                                            // в удобную для чтения строки и складываем их в переменную operationDataBase
     }
-    echo json_encode($operationDataBase);                                                           // выводим результат в виде массива строк 
+    echo json_encode($operationDataBase);                                                                   // выводим результат в виде массива строк 
 ?>
--- Classes/Connect.php
+++ Classes/Connect.php
@@ -1,16 +1,25 @@
 <?php
+    // создаём новый класс
     class Connect {
+        // объявляем локальную переменную, доступную только внутри данного класса
         private $connectionDataBase;
+        // создаём функцию для соединения с БД, которая будет вызываться всякий раз, когда вызывается данный класс
         function __construct() {
             $this->connectionDataBase = mysqli_connect("localhost", "root", "root", "calcBase");
         }
+        // создаём функцию для вставки алгебраической операции в базу данных:
         function saveResultToDataBase($numberOne, $calcOperation, $numberTwo, $calcResultDataBase) {
             mysqli_query($this->connectionDataBase, "INSERT INTO `Operations` (`number_one`, `operation`, `number_two`, `result`) VALUES ('" . $numberOne . "', '" . $calcOperation . "', '" . $numberTwo . "', '" . $calcResultDataBase . "')");
         }
+        // создаём функцию для загрузки в массив нужного количества арифметических операций из базы данных:
         function loadResultsFromDataBase($numberOfOperations) {
+            // объявляем пустой массив
             $DataBase = [];
+            // делаем запрос в БД
             $queryDataBase = mysqli_query($this->connectionDataBase, "SELECT * FROM `Operations` ORDER BY `id` DESC");
+            // обнуляем счётчик количества арифметических операций
             $i=0;  
+            // цикл извлечения данных из БД в массив, прерывается как только достигли нужного количества записей:
             while ($row = mysqli_fetch_assoc($queryDataBase)) {
                 $DataBase[]= $row["number_one"]  . " " . $row["operation"] . " " . $row["number_two"] . " = " . $row["result"];  
                 $i++;
