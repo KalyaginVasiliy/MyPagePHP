800f721687f1b36ca22f2aa54ce47c864eab9d18
KalyaginVasiliy <68701581+KalyaginVasiliy@users.noreply.github.com>
2024-03-05 09:55:48 +0300

    Update resultFromSession.php

--- resultFromSession.php
+++ resultFromSession.php
@@ -1,5 +1,5 @@
 <?php
     session_start();                   // открываем сессию
-    echo "Последняя операция на калькуляторе такая: " . $_SESSION['Operation'];
+    echo "Последняя операция на калькуляторе такая: " . implode($_SESSION['Operation']);
     
 ?>
